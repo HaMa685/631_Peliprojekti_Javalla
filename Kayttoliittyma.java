fec43991b13ca8d3648f80e207bd89ab77e5b1b3
Hanna Marttila <hanna.marttila1@gmail.com>
2021-12-02 13:05:18 +0200

    karsittu ylimääräistä.

--- Kayttoliittyma.java
+++ Kayttoliittyma.java
@@ -7,32 +7,25 @@ public class Kayttoliittyma {
     Tiedostonluku sanat=new Tiedostonluku();
 
     public Kayttoliittyma(Scanner lukija) {
-        this.lukija = lukija;
-        
-        
+        this.lukija = lukija;    
     }
-
+    
     public void kaynnista () {
         String sana = sanat.haeSana();
+        
+        System.out.println("Pelataanko Hirsipuuta?");
+       
+        while(true) {           
+            System.out.println("Arvaa kirjain! (x- lopettaa pelin)");
+            String kirjain = lukija.nextLine();
+           
+            kirjaimet.tarkastaTulosta(sana, kirjain);
+            System.out.println();
 
-        while(true) {
-            System.out.println("Pelataanko Hirsipuuta?");
-            System.out.println(" 1 - Aloittaa pelin.");
-            System.out.println(" x - En halua pelata");
-
-            String komento = lukija.nextLine();
-
-            if (komento.equals("x")) {                
-                System.out.println("Seuraavalla kerralla sitten!");
+            if (kirjain.equals("x")) {                
+                System.out.println("Hei hei!");
                 break;
-            }
-
-            if (komento.equals("1")) {
-                System.out.println("Arvaa kirjain!");
-                String kirjain = lukija.nextLine();
-                
-                kirjaimet.tarkastaTulosta(sana, kirjain);
-            }
+            }           
         }
     }
 }
